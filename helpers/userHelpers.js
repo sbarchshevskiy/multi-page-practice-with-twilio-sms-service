224bf889923605200899c6d2208cc2c5136a7f39
ibrikk <ibrahim.khalilov@yahoo.com>
2021-03-07 18:54:55 +0000

    fix helpers

--- helpers/userHelpers.js
+++ helpers/userHelpers.js
@@ -3,7 +3,7 @@ const getUsers = () => {
   const query = {
    text: `SELECT * FROM users;`
   }
-  return db.query(query).then(res => res.rows)
+  return db.query(query).then(result => result.rows)
 }
   return {
     getUsers
