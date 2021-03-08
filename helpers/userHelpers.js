ac22f9f443616df46937cf413bf76088dca686b4
Catherine Dawe <dawe.cm@gmail.com>
2021-03-08 18:48:46 -0500

    restore order

--- helpers/userHelpers.js
+++ helpers/userHelpers.js
@@ -1,5 +1,4 @@
 module.exports = db => {
-<<<<<<< HEAD
   const getUsers = () => {
     const query = {
       text: `SELECT * FROM users;`
@@ -10,13 +9,3 @@ module.exports = db => {
     getUsers
   };
 };
-=======
-const getUsers = () => {
-  const query = {
-   text: `SELECT * FROM users;`
-  }
-  return db.query(query).then(result => result.rows)
-}
- return getUsers();
-}
->>>>>>> 95501503969b5cf431bd8e6bf4ef3f72ce27be14
