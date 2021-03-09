2d96d702935b6dbdab1ad9ecc4050863a7fae41b
Catherine Dawe <dawe.cm@gmail.com>
2021-03-09 18:43:50 -0500

    add dq.queries for menu

--- routes/menus.js
+++ routes/menus.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const menu  = express.Router();
 
+
 module.exports = (db) => {
+
+  const fetchAllItems = function() {
+    return db.query(`
+    SELECT *
+    FROM menu_items;
+    `)
+      .then(res => res.rows[0]);
+  };
+
+  const fetchSingleItem = function(id) {
+    return db.query(`
+    SELECT item_name
+    FROM menu_items
+    WHERE id= $1`, [id])
+    .then(res=>res.rows[0])
+  };
+
+
    menu.get('/', (req, res) => {
-    res.render('menu'); // to decide on categories whether redirect.
+     const templateVars= {
+      menu : fetchAllItems()
+
+     }
+    res.render('menu',templateVars); // to decide on categories whether redirect.
    });
 
    menu.get('/categories/:category_id', (req, res) => {
