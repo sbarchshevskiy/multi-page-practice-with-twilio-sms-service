fefcaced9b1993f658042f204ffdf76236679523
Catherine Dawe <dawe.cm@gmail.com>
2021-03-10 10:33:59 -0500

    add dashboard with database

--- routes/admin.js
+++ routes/admin.js
@@ -6,8 +6,12 @@ module.exports = (db) => {
 
   const fetchAllOrders = function() {
     return db.query(`
-    SELECT *
-    FROM orders;
+      SELECT users.name,users.phone_number, orders.id, order_menu_items.quantity,menu_items.name as item, (menu_items.price * order_menu_items.quantity) as total_price
+      FROM users
+      JOIN orders ON users.id = user_id
+      JOIN order_menu_items ON orders.id = order_id
+      JOIN menu_items ON menu_items.id = menu_item_id
+      GROUP BY users.name,users.phone_number,orders.id ,order_menu_items.quantity, menu_items.name ,total_price;
     `)
       .then(res => res.rows);
   };
