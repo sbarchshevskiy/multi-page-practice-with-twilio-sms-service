f3f3eaf716d429e98e2749ecde322a0ae2802081
Catherine Dawe <dawe.cm@gmail.com>
2021-03-10 21:49:11 -0500

    add data to menu_items

--- db/seeds/03-menu_items.sql
+++ db/seeds/03-menu_items.sql
@@ -1,6 +1,51 @@
 insert into menu_items
-values(1, 1, 'cheese burger', 10, 'just like your mamma cooked', 25, 'https://bit.ly/3cfrYCZ');
+values(1, 1, 'Cheeseburger Burger', 12, 'This scrumptious burger will make your day. Comes with a slice of Bacon for exta love.', 25, 'https://bit.ly/3copv9g');
 insert into menu_items
-values(2, 1, 'double cheese burger', 10, 'just like your mamma cooked', 25, 'https://bit.ly/3cfrYCZ');
+values(2, 1, 'Double Cheese Burger', 17, 'This master piece comes all dressed, and extra napkins.', 25, 'https://bit.ly/3cfrYCZ');
 insert into menu_items
-values(3, 1, 'triple cheese burger', 10, 'just like your mamma cooked', 25, 'https://bit.ly/3cfrYCZ');
+values(3, 1, 'Sirloin Burger', 22, 'This fancy patty is for the fine palets.', 25, 'https://bit.ly/2PO8z4x');
+insert into menu_items
+values(4, 1, 'Classic Burger with fries', 18, 'When one of our classic is not enough...', 30, 'https://bit.ly/2OHQN2x');
+insert into menu_items
+values(5, 1, 'Cheese Tower Buger', 18, 'When a Cheeseburger is not enough anymore', 30, 'https://bit.ly/3bAqEvy');
+insert into menu_items
+values(6, 1, 'Chicken Burger with a twist', 15, 'Chicken that will change your life. ', 25, 'https://bit.ly/3l4fwdc');
+insert into menu_items
+values(7, 1, 'Chicken Wings', 12, 'Six wings spiced to perfection.', 20, 'https://bit.ly/3czOiaT');
+insert into menu_items
+values(8, 1, 'Classic Hot Dog', 6, 'You can buy one or 3, you will thank us', 12, 'https://bit.ly/3l5Bug6');
+insert into menu_items
+values(9, 1, 'Classic Chocolate Milkshake', 8, 'For the chocolate lovers', 10, 'https://bit.ly/3vdmr8N');
+insert into menu_items
+values(10, 1, 'Classic Strawberry Milkshare', 8, 'If chocolate is too sweet for you, this fruity Milkshake will do the trick.', 10, 'https://bit.ly/2OnFrRp');
+insert into menu_items
+values(11, 1, 'Classic Vanilla Milkshake', 8, 'Sweet and Vanilla flavored.', 10, 'https://bit.ly/3t7Zm5E');
+insert into menu_items
+values(12, 1, 'Pepperoni Pizza by the Slice', 15, 'We only sell it by the slice so you come back and get more.', 25, 'https://bit.ly/38qFLFW');
+insert into menu_items
+values(13, 2, 'Sweet Patato Fries', 6, 'When you need some extra fries but want to be fancy. ', 10, 'https://bit.ly/3bAmWSD');
+insert into menu_items
+values(14, 2, 'Fries', 4, 'When you need some extra fries. ', 10, 'https://bit.ly/30xoDKk');
+insert into menu_items
+values(15, 2, 'Onion Rings', 7, 'What can we say about fried onions ?', 15, 'https://bit.ly/3etgrD2');
+insert into menu_items
+values(16, 2, 'Mozzarella Sticks', 7, 'Six sticks to share with your friends', 15, 'https://bit.ly/3cqJCUG');
+insert into menu_items
+values(17, 2, 'Meatless Burger', 12, 'Meat has no chance with this vegetarian patty.', 15, 'https://bit.ly/3vbNQbd');
+insert into menu_items
+values(18, 2, 'Hot No Dog', 12, 'Gotta try our black bean Hot Dog, vegetarian friendly of course.', 15, 'https://bit.ly/2N5gSbc');
+insert into menu_items
+values(19, 2, 'Avo Toast ', 12, 'A little healty vegetarian option', 5, 'https://bit.ly/3eAh0dY');
+insert into menu_items
+values(20, 2, 'Side Salad', 14, 'If you need to get some side of greens with your fries..', 15, 'https://bit.ly/2OjHeXD');
+insert into menu_items
+values(21, 2, 'Green bowl', 15, 'If you need to get some side of greens with your fries..', 15, 'https://bit.ly/3cjULGG');
+insert into menu_items
+values(22, 2, 'Strawberry Shake', 9, 'A dairy free option of our bestseller', 15, 'https://bit.ly/2OjGFNv');
+insert into menu_items
+values(23, 2, 'House Kombucha', 9, 'It is made here, helps digest the rest! ', 5, 'https://bit.ly/3rHUfc8');
+insert into menu_items
+values(24, 2, 'House Lemonade', 9, 'Freshy squeezed', 5, 'https://bit.ly/3rCV3Pp');
+
+
+
