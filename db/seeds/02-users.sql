4f2dfad8e44b39e89f1f868d4be1cbf73bb1cd87
vagrant <sbarchshevskiy@gmail.com>
2021-03-25 17:08:11 +0000

    touch ups

--- db/seeds/02-users.sql
+++ db/seeds/02-users.sql
@@ -1,5 +1,5 @@
 insert into users(id, name, email, password, phone_number, is_owner)
 values(1,'ricardo', 'ricardo@lasvegas.com', 'allInBets', '5559871432', true);
 insert into users(id, name, email, password, phone_number, is_owner)
-values(2, 'michel', 'michel@lasvegas.com', 'hola', '5142586848', false);
+values(2, 'michel', 'michel@lasvegas.com', 'hola', '5142583228', false);
 
